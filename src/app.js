f39af7423e4b1bb4239e0da6a3b3b95389d4ee70
Colin Stodd <cbstodd@gmail.com>
2015-11-22 20:58:41 -0500

    fixed a bug

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+      posts = require('./mock/posts.json');
 
 var app = express();
 
 //Creates a server request.
 app.get('/', function (req, res) {
-  res.send("<h1>I love learning to code.</h1>");
+  res.send("<h1>Im creating my first express app! Looking good</h1>");
+});
+
+app.get('/blog', function (req, res) {
+  res.send(posts);
+});
+
+app.get('/blog/:title', function (req, res) {
+
+  var title = req.params.title;
+  var post = posts[title];
+  res.send(post);
 });
 
 //tells server request to listen on '3000'.
-app.listen(3000, function () {
-  console.log('-- The frontend server is running on port (3000) --');
+app.listen(3772, function () {
+  console.log('*** The frontend server is running on port (3772) ***');
 });
